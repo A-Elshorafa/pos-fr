ad3c0a1b189757c1df557414113b8b6fdd14fe7b
Abdelrahman <abdelrahman_elshorafa@outlook.com>
2025-11-17 23:31:41 +0200

    feat(login): update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  darkMode: false,
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Afacad Flux", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+        display: ["Nunito Sans", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          blue: "#000842",
+        },
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light"],
+  },
+};
