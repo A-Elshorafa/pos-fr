4f8a685a7c61780ffec971ab471de2902eb3b616
Abdelrahman <abdelrahman_elshorafa@outlook.com>
2025-11-18 07:39:01 +0200

    feat(settings): add the settings

--- src/app/features/settings/settings.component.ts
+++ src/app/features/settings/settings.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { CardComponent } from "../../shared/components/card/card";
 
 @Component({
   selector: "app-settings",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardComponent],
   templateUrl: "./settings.component.html",
 })
 export class SettingsComponent {}
